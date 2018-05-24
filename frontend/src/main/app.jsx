f1715c843bfaf3e72b8bdfed78669b75c90277f5
Ronaldo Guastalli <guastalli31@gmail.com>
2018-05-24 01:51:42 -0300

    configurando rotas react-router

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,9 +4,10 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 
 //meus componentes
-import Todo from '../todo/todo'
-import About from '../about/about'
+    // import Todo from '../todo/todo' 
+    // import About from '../about/about'
 import Menu from '../template/menu'
+import Routes from './routes';
 
 /* 
 este arquivo é usado em index.jsx, por exemplo,
@@ -15,7 +16,6 @@ ReactDOM.render(<App />, document.getElementById('app'))
 export default props => (
     <div className='container'>
         <Menu />
-        <Todo />
-        <About />
+        <Routes />
     </div>
 )
