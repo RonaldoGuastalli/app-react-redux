add661d0780566b85a9b8a1a0ec41a037911ce25
Ronaldo Guastalli <guastalli31@gmail.com>
2018-05-24 01:02:44 -0300

    componente menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,8 +2,11 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+
+//meus componentes
 import Todo from '../todo/todo'
 import About from '../about/about'
+import Menu from '../template/menu'
 
 /* 
 este arquivo é usado em index.jsx, por exemplo,
@@ -11,6 +14,7 @@ ReactDOM.render(<App />, document.getElementById('app'))
 */
 export default props => (
     <div className='container'>
+        <Menu />
         <Todo />
         <About />
     </div>
