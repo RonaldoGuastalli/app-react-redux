4b0395d6a7e8ef88aad6bad9382e96e95881d016
Ronaldo Guastalli <guastalli31@gmail.com>
2018-05-27 08:42:38 -0300

    consulta e exclusão

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -6,6 +6,7 @@ import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
+// url para acesso ao backend
 const URL = 'http://localhost:3030/api/todos'
 
 export default class Todo extends Component {
@@ -15,20 +16,30 @@ export default class Todo extends Component {
 
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
 
+        this.refresh()
+    }
+
+    //refresh a lista
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`).then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
     }
 
     handleAdd() {
-        // console.log(this.state.description)
         const description = this.state.description
-        axios.post(URL, { description })
-            .then(resp => console.log('Funcionou!'))
+        axios.post(URL, { description }).then(resp => this.refresh())
     }
 
     /* handleChange recebe um evento (e), devolve o valor (value) description
     que nada mais é do que esta na entrada do input */
     handleChange(e) {
         this.setState({ ...this.state, description: e.target.value })
+    }
+
+    //remove e atualiza a lista de acordo com metodo refresh
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh())
     }
 
 
@@ -40,7 +51,9 @@ export default class Todo extends Component {
                     description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} />
-                <TodoList />
+                <TodoList
+                    list={this.state.list}
+                    handleRemove={this.handleRemove} />
             </div>
         )
     }
