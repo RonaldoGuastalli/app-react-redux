4dcbacac36e6bb4c050be498d2791f97231dc65d
Ronaldo Guastalli <guastalli31@gmail.com>
2018-05-31 09:07:07 -0300

    Limpar Formulário

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { add, changeDescription, search } from './todoActions'
+import { add, changeDescription, search, clear } from './todoActions'
 
 /* 
     onChande -> handleChange -> todo -> handleChange(e) -> 
@@ -24,11 +24,11 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
-        const { add, search, description } = this.props
+        const { add, search, description, clear } = this.props
         if (e.key === 'Enter') {
             e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
-            props.handleClear()
+            clear()
         }
     }
 
@@ -49,7 +49,7 @@ class TodoForm extends Component {
                     <IconButton style='info' icon='search'
                         onClick={() => search()}></IconButton>
                     <IconButton style='default' icon='close'
-                        onClick={this.props.handleClear}></IconButton>
+                        onClick={this.props.clear}></IconButton>
                 </Grid>
             </div>
         )
@@ -62,7 +62,7 @@ actionCreators - An action creator, or an object whose values are action creator
 dispatch function available on the Store instance - todo em reducers.js.
 */
 const mapStateToProps = state => ({ description: state.todo.description })
-const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search, clear }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
 
 
